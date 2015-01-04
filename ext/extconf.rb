d6fdc4a2dbbf559d84717e0e4226a5b372706ce1
Watson <watson1978@gmail.com>
2015-01-04 12:22:20 +0900

    fix filename

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,8 +2,8 @@
 require 'fileutils'
 
 dir = File.expand_path("~/Library/RubyMotion/command/")
-src = File.expand_path(File.join(File.dirname(__FILE__), "../command/motion-doc.rb"))
-dst = File.join(dir, "motion-doc.rb")
+src = File.expand_path(File.join(File.dirname(__FILE__), "../command/motion-validate.rb"))
+dst = File.join(dir, "motion-validate.rb")
 
 FileUtils.mkdir_p(dir) unless File.exist?(dir)
 FileUtils.rm dst if File.exist?(dst)
