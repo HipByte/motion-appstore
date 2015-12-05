ae16849ee023adbd7de3b26a05e0a66c4f80df53
Watson <watson1978@gmail.com>
2015-12-05 10:52:41 +0900

    [#10] create '/usr/local/itms'

--- ext/installer.rb
+++ ext/installer.rb
@@ -12,4 +12,11 @@ def install_plugins
     FileUtils.mkdir_p(dir) unless File.exist?(dir)
     FileUtils.ln_sf src, dst
   end
+
+  # http://stackoverflow.com/questions/28461768/xcode-organizer-trying-to-access-transporter-at-wrong-directory-pathhttp://stackoverflow.com/questions/28461768/xcode-organizer-trying-to-access-transporter-at-wrong-directory-path
+  itms_src = '/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/itms'
+  itms_dst = '/usr/local/itms'
+  if File.exist?(itms_src) && !File.exist?(itms_dst)
+	FileUtils.ln_sf itms_src, itms_dst
+  end
 end
